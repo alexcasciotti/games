1074c00a9f994ece99fa24c0940f397c96f6ff61
Alex Casciotti <alexc2048@gmail.com>
2022-09-17 19:32:56 -0400

    Madlibs v5

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,28 @@
+import random
 #Madlibs is a game where the player completes a story by replacing missing words with their own unique choices.
 
+story_bank = ['The (noun) is (adjective)! What a wonderful (noun)!']
+story = random.choice(story_bank)
+
+def start():
+    print(f'\nWelcome to madlibs!\nHere is your story...\n{story}')
+    print(build_story())
+
 def build_story():
-    print('\nWelcome to madlibs!\nHere is your story...\nThe sky is (adjective)! What a wonderful (noun)!\n')
-    adj1 = input("Enter an adjective: ")
-    noun1 = input("Enter an noun: ")    
-    return f'\nThe sky is {adj1}! What a wonderful {noun1}!\n'
-    
+    new_story = ''
+    index = 0
+    while(index < len(story)):
+        if(story[index] == '('):
+            pos = ''
+            while(story[index] != ')'):
+                pos += story[index]
+                index += 1
+            pos += story[index]
+            index += 1
+            missing_word = input(f'Enter a {pos}...\n').upper()
+            for x in missing_word:
+                new_story += x
+        new_story += story[index]
+        index += 1     
+    return new_story
+start()
