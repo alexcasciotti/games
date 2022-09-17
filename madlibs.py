6c2e4c2508637f0db7492aee09db5af1067fabdf
Alex Casciotti <alexc2048@gmail.com>
2022-09-17 18:01:18 -0400

    Madlibs v3

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,9 @@
 #Madlibs is a game where the player completes a story by replacing missing words with their own unique choices.
 
-adj1 = input("Enter an adjective: ")
-noun1 = input("Enter an noun: ")
-print(f"The sky is {adj1}! What a wonderful {noun1}!") #madlib story
+def build_story():
+    print('Welcome to madlibs!\nHere is your story...\n')
+    print('The sky is (adjective)! What a wonderful (noun)!')
+    adj1 = input("Enter an adjective: ")
+    noun1 = input("Enter an noun: ")    
+    return f'\nThe sky is {adj1}! What a wonderful {noun1}!\n'
+    
