09db91b512c8f3e3e957f3d43ed9b8914c611a36
Alex Casciotti <alexc2048@gmail.com>
2022-09-17 17:08:59 -0400

    NUmber guesser v3

--- number_guesser.py
+++ number_guesser.py
@@ -1,24 +1,31 @@
 import random
 
 def guess(x):
-    lives = 3
-    random_number = random.randint(1,x)
-    guess = 0
-    while(guess != random_number):
-        if(lives <= 0):
-            print('You lose!')
-            break
-        print(f'Lives : {lives}')
-        guess = int(input(f'Guess a number between 1 and {x}.\n'))
-        print(f'\nChoice : {guess}')
-        if(guess > random_number):
-            lives -= 1
-            print('Wrong!\nTry lower...\n')
-        if(guess < random_number):
-            lives -= 1
-            print('Wrong!\nTry higher...\n')
-    if(lives > 0):
-        print('Congrats, you win!')
+        lives = 3
+        random_number = random.randint(1,x)
+        guess = 0
+        while(guess != random_number):
+            if(lives <= 0):
+                print('You lose!')
+                break
+            print(f'Lives : {lives}')
+            guess = int(input(f'Guess a number between 1 and {x}.\n'))
+            print(f'\nChoice : {guess}')
+            if(guess > random_number):
+                lives -= 1
+                print('Wrong!\nTry lower...\n')
+            if(guess < random_number):
+                lives -= 1
+                print('Wrong!\nTry higher...\n')
+        if(lives > 0):
+            print('Congrats, you win!')
 
-upper_bound = int(input('You get 3 lives...\nEnter an upper bound: '))
-guess(upper_bound)
+
+exit = 0
+while(exit == 0):
+    try:    
+        upper_bound = int(input('You get 3 lives...\nEnter an upper bound: '))
+        guess(upper_bound)
+        exit = 1
+    except ValueError:
+        print('The upper bound must be an integer.')
