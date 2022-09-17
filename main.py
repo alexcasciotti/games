acd3d90893ed3268e2e7f0548cadf454ce68c610
Alex Casciotti <alexc2048@gmail.com>
2022-09-17 18:01:34 -0400

    Main app v2

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+from ast import expr_context
+import madlibs
 import number_guesser
+import rock_paper_scissors
 
-# number guesser application
+
 exit = 0
 while(exit == 0):
-    try:    
-        upper_bound = int(input('You get 3 lives...\nEnter an upper bound: '))
-        number_guesser.guess(upper_bound)
+    game_choices = {1 : 'madlibs.py', 2 : 'number_guesser.py', 3 : 'rock_paper_scissors.py'}
+    print(f'Games:\n')
+    for x in game_choices:
+        print(f'{x} : {game_choices[x]}\n')
+    user_game_choice = int(input('What game would you like to play?\nSelect the game number(0 to exit) and press \'Enter\' to begin...\n'))
+    if(user_game_choice == 0):
         exit = 1
-    except ValueError:
-        print('The upper bound must be an integer.')
+    if(user_game_choice == 1):
+        print(madlibs.build_story())
+        advance = input('Continue? (Y or N)').lower()
+        if(advance == 'n'):
+            exit = 1
+    if(user_game_choice == 2):
+        exit = 0
+        while(exit == 0):
+            try:
+                upper_bound = int(input('Welcome to the number guessing game!\nType an upper bound and press \'Enter\' to begin...\n'))
+                number_guesser.guess(upper_bound)
+                advance = input('Continue? (Y or N)').lower()
+                if(advance == 'n'):
+                    exit = 1
+            except ValueError:
+                print('Please enter an integer')
+    if(user_game_choice == 3):
+        advance = input('Continue? (Y or N)').lower()
+        if(advance == 'n'):
+            exit = 1
+        
