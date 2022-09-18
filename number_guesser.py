4f0c640726b4c7461b14cdf624a3758a58f5571e
Alex Casciotti <alexc2048@gmail.com>
2022-09-17 22:45:24 -0400

    Number guessor v7

--- number_guesser.py
+++ number_guesser.py
@@ -1,25 +1,33 @@
 import random
 
-def guess(x):
-        lives = 10
-        random_number = random.randint(1,x)
-        guess = 0
-        while(guess != random_number):
-            if(lives <= 0):
-                break
-            print(f'Lives : {lives}')
-            guess = int(input(f'Guess a number between 1 and {x}.\n'))
-            print(f'\nChoice : {guess}')
-            if(guess > random_number):
-                lives -= 1
-                print('Wrong!\nTry lower...\n')
-            if(guess < random_number):
-                lives -= 1
-                print('Wrong!\nTry higher...\n')
+def guess(x, difficulty):
+    lives = (3, 6, 9)
+    if(difficulty == '1'):
+        lives = lives[0]
+    if(difficulty == '2'):
+        lives = lives[1]
+    if(difficulty == '3'):
+        lives = lives[2]
+    if(difficulty != '1' and difficulty != '2' and difficulty != '3'):
+        return
+    random_number = random.randint(1,x)
+    guess = 0
+    while(guess != random_number):
         if(lives <= 0):
-            print('You lose!')
-        else:
-            print('Congrats, you win!')
+            break
+        print(f'Lives : {lives}')
+        guess = int(input(f'Guess a number between 1 and {x}.\n'))
+        print(f'\nChoice : {guess}')
+        if(guess > random_number):
+            print('Wrong!\nTry lower...\n')
+            lives -= 1
+        if(guess < random_number):
+            print('Wrong!\nTry higher...\n')
+            lives -= 1
+    if(lives <= 0):
+        print(f'You lose!\nThe number was {random_number}!\n')
+    else:
+        print('Congrats, you win!')
 
 
 
